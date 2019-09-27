6952da1c3d30b0b4325b6c3a13f4364de726c1f4
Carlos Eduardo Duarte Schwalm <krlsedu@gmail.com>
2019-09-27 20:52:43 -0300

    Inicial

---
+++ src/main/java/br/com/krlsedu/onibusPoa/OnibusPoaApplication.java
@@ -0,0 +1,13 @@
+package br.com.krlsedu.onibusPoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnibusPoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnibusPoaApplication.class, args);
+	}
+
+}
