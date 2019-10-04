0d51ba25e37b74de804c25007f46be9202e5ae69
Carlos Eduardo Duarte Schwalm <krlsedu@gmail.com>
2019-10-04 05:12:28 -0300

    CRUD pronto e documentado. Linha totalmente reativa

--- src/main/java/br/com/krlsedu/onibusPoa/configs/ReactiveMongoConfig.java
+++ src/main/java/br/com/krlsedu/onibusPoa/configs/ReactiveMongoConfig.java
@@ -1,23 +1,23 @@
-//package br.com.krlsedu.onibusPoa.configs;
-//
-//import com.mongodb.reactivestreams.client.MongoClient;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
-//
-//@Configuration
-//public class ReactiveMongoConfig {
-//
-//	private final MongoClient mongoClient;
-//
-//	@Autowired
-//	public ReactiveMongoConfig(MongoClient mongoClient) {
-//		this.mongoClient = mongoClient;
-//	}
-//
-//	@Bean
-//	public ReactiveMongoTemplate reactiveMongoTemplate() {
-//		return new ReactiveMongoTemplate(mongoClient, "mongo");
-//	}
-//}
+package br.com.krlsedu.onibusPoa.configs;
+
+import com.mongodb.reactivestreams.client.MongoClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+
+@Configuration
+public class ReactiveMongoConfig {
+	
+	private final MongoClient mongoClient;
+	
+	@Autowired
+	public ReactiveMongoConfig(MongoClient mongoClient) {
+		this.mongoClient = mongoClient;
+	}
+	
+	@Bean
+	public ReactiveMongoTemplate reactiveMongoTemplate() {
+		return new ReactiveMongoTemplate(mongoClient, "mongo");
+	}
+}
